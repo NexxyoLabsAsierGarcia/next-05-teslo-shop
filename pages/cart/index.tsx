ad0c47c8c37904aa648bfa701d6ff9c9a57833c0
Asier Garcia | Web dpto <asier.garcia@nexxyolabs.com>
2023-10-17 15:16:48 +0200

    video 195

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,19 +1,33 @@
+import { CartList, OrderSummary } from "@/components/cart"
 import { ShopLayout } from "@/components/layouts"
-import { Card, Grid, Typography } from "@mui/material"
+import { Box, Button, Card, CardContent, Divider, Grid, Typography } from "@mui/material"
 
 const CartPage = () => {
   return (
     <ShopLayout title="Cart" pageDescription="Teslo shopping cart">
-        <Typography variant="h1" component="h1">
+        <>
+            <Typography variant="h1" component="h1">Cart</Typography>
             <Grid container>
                 <Grid item xs={12} sm={7}>
-                    {/* Cart list */}
+                    <CartList editable />
                 </Grid>
                 <Grid item xs={12} sm={5}>
-                    <Card className="su"></Card>
+                    <Card className="summary-card">
+                        <CardContent>
+                            <Typography variant="h2">Orden</Typography>
+                            <Divider sx={{ my:1 }} />
+
+                            <OrderSummary />
+
+                            <Box sx={{ mt: 3 }}>
+                                <Button color="secondary" className="circular-btn" fullWidth>Checkout</Button>
+                            </Box>
+
+                        </CardContent>
+                    </Card>
                 </Grid>
             </Grid>
-        </Typography>
+        </>
     </ShopLayout>
   )
 }
